7164b43839468bd0b55787dee257ed1ad16667ff
superhetja <42982342+superhetja@users.noreply.github.com>
2021-04-27 15:11:33 +0000

    first commit

--- pages/index.js
+++ pages/index.js
@@ -4,47 +4,32 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Koktailvagninn</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Kokteilvagninn!
         </h1>
 
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Við komum með partýið til þín
         </p>
 
         <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <h2>Ertu að fara að halda partý eða veislu?</h2>
+          <p>Ertu einn af þeim sem veit aldrei hversu mikið á að kaupa af drykkum og endar því með fulla skápa af óopnuðu víni</p>
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
+{/*           <a
             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className="card"
           >
             <h3>Deploy &rarr;</h3>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Instantly dploy your Next.js site to a public URL with Vercel.
             </p>
-          </a>
+          </a> */}
         </div>
       </main>
 
@@ -67,6 +52,8 @@ export default function Home() {
           flex-direction: column;
           justify-content: center;
           align-items: center;
+          background-image: url("../cocktail.jpg");
+          
         }
 
         main {
